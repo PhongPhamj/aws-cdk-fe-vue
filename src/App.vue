dcc2768baa02e147134ae787838e259714dee2eb
PhongPhamj <phongvupham1110@gmail.com>
2024-12-14 15:31:13 +0700

    test pipeline 1

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,7 @@ import HelloWorld from './components/HelloWorld.vue'
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
-      <HelloWorld msg="Welcome to Cloudmancer Media!" />
+      <HelloWorld msg="TEST PIPELINE 14/12/2024 15:29" />
 
       <nav>
         <RouterLink to="/">Home</RouterLink>
